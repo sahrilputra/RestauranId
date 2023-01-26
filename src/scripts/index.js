f63a440366d9388ca38724242a94c27b9f51cda6
sahrilputra <iam.sahrilputra@gmail.com>
2023-01-26 22:55:59 +0800

    header styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
+const data = require('../DATA.json');
 
+console.log(data)
 console.log('Hello Coders! :)');
