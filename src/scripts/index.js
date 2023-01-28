2eef41983e4d24d22519a2317fb989636d255e36
sahrilputra <iam.sahrilputra@gmail.com>
2023-01-28 22:04:40 +0800

    fetching restaurant

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,40 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
 import '../styles/navigation.scss';
-const data = require('../DATA.json');
+import DATA from '../DATA.json';
+console.log(DATA)
+const getData=(data)=>{
+    let cardHtml = '';
+    data.restaurants.forEach((resto, i) =>{
+        cardHtml += `
+      <div tabindex="0" class="card">
+        <div class="card__imgCont">
+          <img tabindex="0" class="card__img" alt="${resto.name}" src="${resto.pictureId}"/>
+          <span tabindex="0" class="card__rating">
+            <i title="ratings" class="fa fa-star"></i>
+            <span>${resto.rating}</span>
+          </span>
+        </div>
 
-console.log(data)
-console.log('Hello Coders! :)');
+        <div tabindex="0" class="card__content">
+          <p class="card__title">${resto.name} - ${resto.city}</p>
+          <p class="card__descr">${resto.description}</p>
+        </div>
+      </div>
+      `;
+    })
+
+    document.getElementById('card').innerHTML=cardHtml;
+}
+
+const navControl = () => {
+    const navBar = document.querySelector('.container');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById('navbar').style.background = "#0d0d0d";
+      } else {
+        document.getElementById("navbar").style.background = "none";
+      }
+}
+window.onscroll = function() {navControl()};
+
+getData(DATA);
