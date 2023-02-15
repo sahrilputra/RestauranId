f6546609c7a46053d663046b40609e053e88596a
sahrilputra <iam.sahrilputra@gmail.com>
2023-02-16 03:30:15 +0700

    create like button template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -73,4 +73,21 @@ const createRestoListTemplate = (resto) => `
   </div>
 `;
 
-export { createRestoListTemplate, createRestoDetailTemplate };
+const createUnlikeButtonTemplate = () => `
+  <button aria-label="like this movie" id="likeButton" class="like">
+     <i class="fa fa-heart-o" aria-hidden="true"></i>
+  </button>
+`;
+
+const createLikedButtonTemplate = () => `
+<button aria-label="unlike this movie" id="likeButton" class="like">
+  <i class="fa fa-heart" aria-hidden="true"></i>
+</button>
+`;
+
+export {
+  createRestoListTemplate,
+  createRestoDetailTemplate,
+  createUnlikeButtonTemplate,
+  createLikedButtonTemplate,
+};
